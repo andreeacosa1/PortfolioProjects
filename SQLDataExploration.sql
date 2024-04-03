870e8a8f365580f6b5eb1ad88317621942a41edb
andreeacosa1 <143832490+andreeacosa1@users.noreply.github.com>
2024-04-03 17:51:10 +0100

    Update SQLDataExploration.sql

--- SQLDataExploration.sql
+++ SQLDataExploration.sql
@@ -1,38 +1,54 @@
-
--- Select Data that we are going to be starting with
-
 Select Location, date, total_cases, new_cases, total_deaths, population
 From PortfolioProject..CovidDeaths
 Where continent is not null 
 order by 1,2
 
 
+
 -- Total Cases vs Total Deaths
---Likelihood ofdying if you contract covid in your country
 
 Select location, date, total_cases, total_deaths, (CONVERT(float, total_deaths) / NULLIF(CONVERT(float, total_cases),0))*100 AS DeathPercentage 
 From PortfolioProject..CovidDeaths
-where location like '%mania%'
 order by 1,2
 
 
---Looking at Total cases vs Population
+
+-- Total Cases vs Total Deaths in Romania
+
+Select location, date, total_cases, total_deaths, (CONVERT(float, total_deaths) / NULLIF(CONVERT(float, total_cases),0))*100 AS DeathPercentage 
+From PortfolioProject..CovidDeaths
+where location='Romania'
+order by 1,2
+
+
+
+--Total cases vs Population
 
 Select location, date, total_cases, population, (CONVERT(float, total_cases) / NULLIF(CONVERT(float, population),0))*100 AS CasePerPopulation 
 From PortfolioProject..CovidDeaths
-where location like '%mania%'
 order by CasePerPopulation desc
 
 
---Looking at countries with Highest Infection Rate compared to Population
 
-Select location, population, MAX(total_cases) as HighestInfectionCount, MAX(CONVERT(float, total_cases) / NULLIF(CONVERT(float, population),0))*100 AS CasePerPopulation 
+--Total cases vs Population in Romania
+
+Select location, date, total_cases, population, (CONVERT(float, total_cases) / NULLIF(CONVERT(float, population),0))*100 AS CasePerPopulation 
+From PortfolioProject..CovidDeaths
+where location='Romania'
+order by CasePerPopulation desc
+
+
+
+--Countries with Highest Infection Rate compared to Population
+
+Select location, population, MAX(convert(float, total_cases)) as HighestInfectionCount, MAX(CONVERT(float, total_cases) / NULLIF(CONVERT(float, population),0))*100 AS PercentPopulationInfected 
 From PortfolioProject..CovidDeaths
 Group by Location, Population
-Order by HighestInfectionCount desc
+Order by PercentPopulationInfected desc
 
 
---Looking at countries with the Highest death count per population
+
+--Countries with the Highest deaths
 
 Select location, MAX(cast(total_deaths as int)) as TotalDeathCount
 From PortfolioProject..CovidDeaths
@@ -42,7 +58,7 @@ order by TotalDeathCount desc
 
 
 
---Sorted by continent with the highest death count per population
+--Continents with the highest death count 
 
 Select continent, MAX(cast(total_deaths as int)) as TotalDeathCount
 From PortfolioProject..CovidDeaths
@@ -62,8 +78,9 @@ join PortfolioProject..CovidVaccinations vac
 	 and dea.date = vac.date
 
 
---Looking at total population vs vaccinations
 
+
+--Looking at new vaccinations
 
 Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations, SUM(cast(vac.new_vaccinations as int)) over (Partition by dea.location order by dea.location, dea.date) as RollingPeopleVaccinated
 from PortfolioProject..CovidDeaths dea
@@ -75,24 +92,23 @@ join PortfolioProject..CovidVaccinations vac
 
 
 
-	 --USE CTE 
-
+--Using CTE 
 
 With PopvsVac (Continent, Location, Date, Population, New_Vaccinations, RollingPeopleVaccinated)
 as
 (
 Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
-, SUM(CONVERT(int,vac.new_vaccinations)) OVER (Partition by dea.Location Order by dea.location, dea.Date) as RollingPeopleVaccinated
---, (RollingPeopleVaccinated/population)*100
+, SUM(CONVERT(float,vac.new_vaccinations)) OVER (Partition by dea.Location Order by dea.location, dea.Date) as RollingPeopleVaccinated
 From PortfolioProject..CovidDeaths dea
 Join PortfolioProject..CovidVaccinations vac
 	On dea.location = vac.location
 	and dea.date = vac.date
-where dea.continent is not null 
---order by 2,3
+where dea.continent <> '' 
 )
-Select *, (RollingPeopleVaccinated/Population)*100
+
+Select *, (RollingPeopleVaccinated/NULLIF(CONVERT(float, population),0))*100 
 From PopvsVac
+
 
 
 
@@ -116,22 +132,49 @@ join PortfolioProject..CovidVaccinations vac
      on dea.location = vac.location
 	 and dea.date = vac.date
 	 where dea.continent <> ''
-	 
-
-Select *, (RollingPeopleVaccinated/NULLIF(Population,0))*100
-from #PercentPopulationVaccinated
 
 
+SELECT *, (CAST(RollingPeopleVaccinated AS DECIMAL(18, 2)) / NULLIF(Population, 0)) * 100 AS PercentageVaccinated
+FROM  #PercentPopulationVaccinated;
 
 
---Creating views 
-
-Create View PercentPopulationVaccinated as 
-Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations, SUM(cast(vac.new_vaccinations as int)) over (Partition by dea.location order by dea.location, dea.date) as RollingPeopleVaccinated
-from PortfolioProject..CovidDeaths dea
-join PortfolioProject..CovidVaccinations vac
-     on dea.location = vac.location
-	 and dea.date = vac.date
-	 where dea.continent <> ''
 
 
+
+-- Queries for Tableau
+
+-- 1. 
+
+Select SUM(cast(new_cases as float)) as total_cases, SUM(cast(new_deaths as float)) as total_deaths, SUM(cast(new_deaths as float))/SUM(cast(New_Cases as float))*100 as DeathPercentage
+From PortfolioProject..CovidDeaths
+where continent is not null 
+order by 1,2
+
+
+-- 2. 
+
+-- European Union is part of Europe
+
+Select location, SUM(cast(new_deaths as int)) as TotalDeathCount
+From PortfolioProject..CovidDeaths
+Where continent = '' 
+and location not in ('World', 'European Union', 'International')
+Group by location
+order by TotalDeathCount desc
+
+
+-- 3.
+
+Select Location, Population, MAX(cast(total_cases as float)) as HighestInfectionCount,  Max(cast(total_cases as float)/NULLIF(cast(population as float),0))*100 as PercentPopulationInfected
+From PortfolioProject..CovidDeaths
+Group by Location, Population
+order by PercentPopulationInfected desc
+
+
+
+-- 4.
+
+Select Location, Population,date, MAX(cast(total_cases as float)) as HighestInfectionCount,  Max(cast(total_cases as float)/NULLIF(cast(population as float),0))*100 as PercentPopulationInfected
+From PortfolioProject..CovidDeaths
+Group by Location, Population, date
+order by PercentPopulationInfected desc
